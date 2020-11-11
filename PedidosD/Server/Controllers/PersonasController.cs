d6bc26021161e8e56852b9299a143f2536d93b17
Anthony Brian Jiménez López <60039575+BryantJim@users.noreply.github.com>
2020-11-11 11:23:06 -0400

    Controllers listos

--- PedidosD/Server/Controllers/PersonasController.cs
+++ PedidosD/Server/Controllers/PersonasController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PedidosD.Shared.Models;
 
 namespace PedidosD.Server.Controllers
 {
@@ -11,5 +13,124 @@ namespace PedidosD.Server.Controllers
     [Route("api/[controller]")]
     public class PersonasController : ControllerBase
     {
+        private Contexto contexto;
+
+        public PersonasController(Contexto contexto)
+        {
+            this.contexto = contexto;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Personas>>> GetList()
+        {
+            List<Personas> Lista;
+            try
+            {
+                Lista = await contexto.Personas.ToListAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return Lista;
+        }
+
+
+        private async Task<ActionResult<bool>> Insertar(Personas persona)
+        {
+            bool guardar = false;
+            try
+            {
+                contexto.Personas.Add(persona);
+                guardar = await contexto.SaveChangesAsync() > 0;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return guardar;
+        }
+
+        [HttpPost("{id}")]
+        public async Task<ActionResult<bool>> Eliminar(int id)
+        {
+            bool eliminado = false;
+            try
+            {
+                var persona = await contexto.Personas.FindAsync(id);
+
+                if (persona != null)
+                {
+                    contexto.Personas.Remove(persona);
+                    eliminado = (await contexto.SaveChangesAsync() > 0);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return eliminado;
+        }
+
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Personas>> Buscar(int id)
+        {
+            Personas persona = new Personas();
+            try
+            {
+                var encontrado = await contexto.Personas.FindAsync(id);
+
+                if (encontrado == null)
+                    return new Personas();
+                else
+                    persona = encontrado;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return persona;
+        }
+
+        private async Task<ActionResult<bool>> Modificar([FromBody] Personas persona)
+        {
+            bool modificar = false;
+            try
+            {
+                contexto.Personas.Update(persona);
+                modificar = await contexto.SaveChangesAsync() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return modificar;
+        }
+
+        private bool Existe(int id)
+        {
+            bool existe;
+            try
+            {
+                existe = contexto.Personas.Any(a => a.PersonaId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return existe;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<bool>> Guardar(Personas persona)
+        {
+            if (Existe(persona.PersonaId))
+                return await Modificar(persona);
+            else
+                return await Insertar(persona);
+        }
     }
 }
--- PedidosD/Server/Controllers/PrestamosController.cs
+++ PedidosD/Server/Controllers/PrestamosController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PedidosD.Shared.Models;
 
 namespace PedidosD.Server.Controllers
 {
@@ -11,5 +13,141 @@ namespace PedidosD.Server.Controllers
     [ApiController]
     public class PrestamosController : ControllerBase
     {
+        private Contexto contexto;
+
+        public PrestamosController(Contexto contexto)
+        {
+            this.contexto = contexto;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Prestamos>>> GetList()
+        {
+            List<Prestamos> Lista;
+            try
+            {
+                Lista = await contexto.Prestamos.ToListAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return Lista;
+        }
+
+
+        private async Task<ActionResult<bool>> Insertar(Prestamos prestamo)
+        {
+            bool guardar = false;
+            try
+            {
+                Personas persona = new Personas();
+                persona = await contexto.Personas.FindAsync(prestamo.PersonaId);
+                prestamo.Balance += prestamo.Monto;
+                persona.Balance += prestamo.Balance;
+                contexto.Personas.Update(persona);
+
+                contexto.Prestamos.Add(prestamo);
+                guardar = await contexto.SaveChangesAsync() > 0;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return guardar;
+        }
+
+        [HttpPost("{id}")]
+        public async Task<ActionResult<bool>> Eliminar(int id)
+        {
+            bool eliminado = false;
+            try
+            {
+                var prestamo = await contexto.Prestamos.FindAsync(id);
+
+                if (prestamo != null)
+                {
+                    Personas persona = new Personas();
+                    persona = await contexto.Personas.FindAsync(prestamo.PersonaId);
+                    persona.Balance -= prestamo.Balance;
+                    contexto.Personas.Update(persona);
+
+                    contexto.Prestamos.Remove(prestamo);
+                    eliminado = (await contexto.SaveChangesAsync() > 0);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return eliminado;
+        }
+
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Prestamos>> Buscar(int id)
+        {
+            Prestamos prestamo = new Prestamos();
+            try
+            {
+                var encontrado = await contexto.Prestamos.FindAsync(id);
+
+                if (encontrado == null)
+                    return new Prestamos();
+                else
+                    prestamo = encontrado;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return prestamo;
+        }
+
+        private async Task<ActionResult<bool>> Modificar([FromBody] Prestamos prestamo)
+        {
+            bool modificar = false;
+            try
+            {
+                Personas persona = new Personas();
+                persona = await contexto.Personas.FindAsync(prestamo.PersonaId);
+                prestamo.Balance += prestamo.Monto;
+                persona.Balance += prestamo.Balance;
+                contexto.Personas.Update(persona);
+
+                contexto.Prestamos.Update(prestamo);
+                modificar = await contexto.SaveChangesAsync() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return modificar;
+        }
+
+        private bool Existe(int id)
+        {
+            bool existe;
+            try
+            {
+                existe = contexto.Prestamos.Any(a => a.PrestamoId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return existe;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<bool>> Guardar(Prestamos prestamo)
+        {
+            if (Existe(prestamo.PrestamoId))
+                return await Modificar(prestamo);
+            else
+                return await Insertar(prestamo);
+        }
     }
 }
